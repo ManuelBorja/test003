461514cc1fbd58fd7183b0424fe99d0d07eb1946
Manuel Borja <mborja00@hotmail.com>
2017-08-18 20:41:38 -0500

    Inicio modelado indicador

--- app/Indicador.js
+++ app/Indicador.js
@@ -7,6 +7,7 @@ import {
   ScrollView
 } from 'react-native';
 
+import { Table, TableWraper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 import {Actions} from 'react-native-router-flux';
 
 import Resultado from './Resultado';
@@ -14,8 +15,18 @@ import Resultado from './Resultado';
 export default class Indicador extends React.Component {
   render() {
 
+    const tableHead = ['', 'T1', 'T2', 'T3', 'T4' ];
+    const tableTitle = ['Meta programada', 'Meta ejecutada', 'Presupuesto programado (Nuevos soles)', 'Presupuesto ejecutado (Nuevo soles)', 'Ejecución %'];
+    const tableData = [
+      ['8', '0', '0', '0'],
+      ['8', '0', '0', '0'],
+      ['22400', '0', '0','0'],
+      ['22400', '0', '0','0'],
+      ['25', '25', '25','25'],
+    ];
+
     const dataIndicador = [
-    {'id':1,'titulo':'Km. Camino de Canal mantenido','unidad':'KM','eficacia':'202','eficiencia':'80','ejecucion':'75'}, ];
+    {'id':1,'titulo':'','indicador':'Km. Camino de Canal mantenido','unidad':'KM','eficacia':'202','eficiencia':'80','ejecucion':'75'}, ];
 
     return (
       <View style={styles.container}>
@@ -42,19 +53,38 @@ export default class Indicador extends React.Component {
           {
             dataIndicador.map(res => {
               return (
-                    <View>
-                      <Resultado key={res.id} data={res} />
+                    <View key={res.id}>
                       <View style={styles.tituloSeccion}>
                         <Text style={styles.textoTituloSeccion}>
-                          Unidad {res.unidad}
+                          Indicador
+                        </Text>
+                        <Text style={styles.textoTituloSeccion}>
+                          {res.indicador}
                         </Text>
                       </View>
+                      <View style={styles.tituloSeccion}>
+                        <Text style={styles.textoTituloSeccion}>
+                          Unidad
+                        </Text>
+                        <Text style={styles.textoTituloSeccion}>
+                          {res.unidad}
+                        </Text>
+                      </View>
+                      <Resultado data={res} />
                     </View>
               )
             })
-          
+
           }
         </ScrollView>
+
+        <Table>
+          <Row data={tableHead} flexArr={[1, 2, 1, 1]} style={styles.head} textStyle={styles.text}/>
+          <TableWraper style={{flexDirection: 'row'}}>
+            <Col data={tableTitle} style={styles.title} heightArr={[28,28]} textStyle={styles.text}/>
+            <Rows data={tableData} flexArr={[2, 1, 1]} style={styles.row}/>
+          </TableWraper>
+        </Table>        
 
       </View>
     );
@@ -94,5 +124,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 20,
   },
+
+  head: { height: 40, backgroundColor: '#f1f8ff' },
+  title: { flex: 1, backgroundColor: '#f6f8fa' },
+  row: { height: 30 },
+  text: { textAlign: 'center' },
 
 });
